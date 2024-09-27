24d7e19e989169bf476700b5fbd577e29869f6e7
yashhhmatic <yashhmatic@gmail.com>
2024-09-27 21:25:57 +0530

    Update team2.jsx

--- src/components/team2.jsx
+++ src/components/team2.jsx
@@ -6,7 +6,7 @@ function Team2() {
       name: "Example 1",
       role: "Co-Founder / CEO",
       imageUrl:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        "https://drive.google.com/drive/u/3/home",
     },
     {
       name: "Example 2",
